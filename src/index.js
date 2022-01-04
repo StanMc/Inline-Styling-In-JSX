6e4325dc7c40309eb26673b126b90d8eb66c674a
McBarbar <42481948+StanMc@users.noreply.github.com>
2022-01-03 20:33:30 -0800

    completed module 399 completed module 399

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const customStyle = {
+  color: "blue",
+  fontSize: "20px",
+  border: "3px solid green",
+  backgroundColor: "coral",
+  borderRadius: "25px",
+  boxShadow: "15px 10px",
+  height: "120px",
+  paddingTop: "80px",
+  paddingLeft: "200px"
+};
+
+customStyle.color = "green";
+
 ReactDOM.render(
-  <h1 style={{ color: "red" }}>Hello World!</h1>,
+  <div>
+    <h1 style={{ color: "red" }}>Hello World!</h1>
+    <h1 style={customStyle}>I love blue!</h1>
+  </div>,
   document.getElementById("root")
 );
